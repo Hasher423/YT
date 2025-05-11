39cb8103e5e175ac71e7dea15277b04fef6a247f
HASHER <hasher423@gmail.com>
2025-05-11 10:34:08 +0500

    vercel config

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,6 @@ const app  = require('./app.js');
 
 const server  = http.createServer(app);
 
-server.setTimeout(300000);
 
 server.listen(3000, () =>{
     console.log('Server is running at port 3000');
