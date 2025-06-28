d17afc58a29d846e37b011675a2dfed368543d95
HASHER <hasher423@gmail.com>
2025-06-28 20:48:09 +0500

    gpt fixes

--- Backend/server.js
+++ Backend/server.js
@@ -3,11 +3,11 @@ const app  = require('./app.js');
 
 const server  = http.createServer(app);
 
+const PORT = process.env.PORT || 3000;
 
-server.listen(process.env.PORT, () =>{
-    console.log(`Server is running at port ${process.env.PORT}`);
-})
-
+server.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
 
 
 
