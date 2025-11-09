5b184694f0f5f7f55c773d3f9d1bd14eff4878c7
HASHER <hasher423@gmail.com>
2025-11-09 11:21:38 +0500

    Cache-issue fixed

--- Backend/app.js
+++ Backend/app.js
@@ -29,7 +29,7 @@ app.use(cors(corsOptions));
 
 // Handle preflight OPTIONS requests for all routes
 app.options("*", cors(corsOptions));
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads')) ;
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(express.json({ limit: '500mb' }));
