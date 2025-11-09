72c1af9b6400b8b0392041cfa589f1a7bc217b78
HASHER <hasher423@gmail.com>
2025-11-09 09:52:38 +0500

    Serverless function connection time increased

--- Backend/DBConnection/db.js
+++ Backend/DBConnection/db.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
 function connectToDb() {
-    mongoose.connect(process.env.MONGODBATLAS_URI, {
-    })
-        .then(() => {
-            console.log('✅ Connected to MongoDB');
-        })
-        .catch((err) => {
-            console.error('❌ MongoDB connection failed:', err.message);
-            process.exit(1); // Exit the process if connection fails
-        });
+  mongoose.connect(process.env.MONGODBATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 20000,
+  })
+    .then(() => console.log('✅ Connected to MongoDB'))
+    .catch((err) => {
+      console.error('❌ MongoDB connection failed:', err.message);
+      process.exit(1);
+    });
 }
 
 module.exports = connectToDb;
