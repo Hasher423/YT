6fdb3bedec869566f933e2e86c9f3177bc932f12
HASHER <hasher423@gmail.com>
2025-11-09 09:55:46 +0500

    Debugging added for vercel

--- Backend/DBConnection/db.js
+++ Backend/DBConnection/db.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
-function connectToDb() {
-  mongoose.connect(process.env.MONGODBATLAS_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 20000,
-  })
-    .then(() => console.log('✅ Connected to MongoDB'))
-    .catch((err) => {
-      console.error('❌ MongoDB connection failed:', err.message);
-      process.exit(1);
+async function connectToDb() {
+  try {
+    console.log("Connecting to MongoDB...");
+    await mongoose.connect(process.env.MONGODBATLAS_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 20000,
     });
+    console.log("✅ Connected to MongoDB");
+  } catch (err) {
+    console.error("❌ MongoDB connection failed:", err);
+  }
 }
 
 module.exports = connectToDb;
