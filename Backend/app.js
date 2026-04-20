95658b59a5ebdac2f80fbd8c3df6c6d225a40943
H_asher <hasher423@gmail.com>
2026-04-20 12:30:32 +0500

    Add CORS support for credentials in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
+     res.header("Access-Control-Allow-Credentials", "true");
   }
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cache-Control, Pragma");
